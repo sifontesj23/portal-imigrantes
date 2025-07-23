53697a6232f6249d343bcaa3b63361a630bd1304
Jesus Sifontes <sifontesj231276@gmail.com>
2025-07-23 14:51:30 -0300

    Modifiquei paragrafo

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -2,7 +2,7 @@ export default function Perfil() {
   return (
     <div style={{ padding: "1rem" }}>
       <h2>Área protegida: Perfil do Usuário</h2>
-      <p>Você está logado e pode ver essa página.</p>
+      <p>Você está logado e pode ver essa página de Usuário</p>
     </div>
   );
 }
